497f8e5c09cc4a9bd9e294575400d23be0da8e46
anuj672 <71286830+anuj672@users.noreply.github.com>
2023-05-07 01:22:46 +0530

    with handling cases when no edges are there

--- adjacency_list.cpp
+++ adjacency_list.cpp
@@ -1,35 +1,39 @@
-#adjacency list graph
-#include <iostream>
+#include<iostream>
+#include<list>
 #include<iterator>
-#include<list> 
 using namespace std;
-void disp_list(list<int> adj_list[]){
-    for(int i=0;i<6;i++){
-        cout<<i<<"-->";
-        list<int> :: iterator it;
-        for(it = adj_list[i].begin(); it!=adj_list[i].end();it++){
-            cout<<*it<<",";
+
+void disp_list(list<int> adj[], int n){
+    for(int i=0;i<n;i++){
+        cout<<i<<"--->";
+
+         if(adj[i].empty()){
+            cout<<"No edges"<<endl;
+            continue;
         }
+        list<int> :: iterator it;
+        for(it=adj[i].begin();it!=adj[i].end();it++)
+        cout<<*it;
         cout<<endl;
     }
 }
-void add(list<int> adj_list[], int s, int e){
-    adj_list[s].push_back(e);
-    
-}
+void add_list(list<int> adj[], int s, int e){
+    adj[s].push_back(e);
+    adj[e].push_back(s);
 
+}
 int main()
 {
-    int v=6;
-    list<int> adj_list[v];
-    add(adj_list,0,4);
-    add(adj_list,4,0);
-    add(adj_list,1,2);
-    add(adj_list,2,1);
-    add(adj_list,0,3);
-    add(adj_list,3,0);
-    disp_list(adj_list);
-    cout<<"Hello World";
-
+    int x;
+    cout<<"list size"<<endl;
+    cin>>x;
+    list<int> adj[x];
+  
+    add_list(adj, 1,2);
+    add_list(adj, 1,4);
+    add_list(adj, 2,3);
+    add_list(adj, 3,4);
+    add_list(adj, 3,5);
+    disp_list(adj,x);
     return 0;
 }
